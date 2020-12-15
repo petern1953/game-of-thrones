eb36cee10d67efbd251b5295a84c3f8ba5923bc6
petern1953 <petern1953@gmail.com>
2020-12-15 19:40:39 +0100

    basic files, instructions

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+// Feladatodként a Trónok harca karaktereivel, azaz a karakterek adataival kell dolgoznod.
+// Egy JSON file a 3. évad elejéig tartja nyilván az adatokat, szóval nincs spoiler veszély,
+// hacsak még nem maradt ki az életedből a sorozat.
+
+// Frontend feladatok
+// A karakterek adatait egy weboldalon kell megjeleníteni, ehhez különböző funkciókra is szükség lesz.
+// Minden file, amelyre szükséged lesz, letölthető a feladat leírásának a végén.
+
+// 1. Html és css segítségével hozz létre egy hasonló elrendezésű oldalt! (Ne használj táblázatot!)
+
+// 2. Az ÉLŐ karakterek profilképe és alatta a nevük legyen megjelenítve. 
+// Mivel ez összesen 48 karakter lesz, ezért pontosan 6 sorod legyen, soronként 8 karakterrel. 
+// (A képen látható középen lévő gappel ne foglalkozz). A képek útvonala ott a json objektumban.
+// A térkép a "site" nevű mappában található az "assets"-en belül.
+// A házak ikonja a "houses" mappában található az "assets"-en belül.
+// Ha bármelyik kép hiányzik, használj egy szabadon választott placeholder image-et.
+
+// 3. A karakterek megjelenítése név szerint rendezve történjen!
+
+// 4. Amennyiben egy karakter nevére rákattintok, a jobb oldali sávban jelenjen meg a nagyobb méretű, 
+// filmből kivett képe, a karakter neve, a házának a címere (ha van) és a rövid leírása.
+
+// 5. Amennyiben a keresőmezőbe beírok egy nevet (teljes nevet, kis- és nagybetű-különbség nem számít), 
+// akkor az adott nevű karakterről jeleníti meg az adatokat.
+// Amennyiben nincs ilyen név, írja ki: „Character not found".
+
+// Plusz:
+
+// 1. Font awesome vagy egyéb ikonok használata a keresőmezőnél!
+// 2. Használj saját betűkészletet a szövegek megjelenítéséhez.
+// A karakterek képeinek / nevének szövege legyen valamilyen effekttel ellátva, 
+// 3. - amikor fölé viszem a kurzort
+// 4. - amikor az adott karakter van kiválasztva!
+// 5. Legyen responsive a megjelenés!
+
+'use strict';
